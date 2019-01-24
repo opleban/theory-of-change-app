2c7bbc3e5f8112b07ea24c802bb429d236596cfd
Jonathan Pleban <opleban@gmail.com>
2019-01-24 14:30:51 +0100

    Gets stage titles to display

--- src/App.js
+++ src/App.js
@@ -4,22 +4,50 @@ import './App.css';
 
 class App extends Component {
   render() {
+    const stages = {
+      title: 'Participatory Budget',
+      style: 'default',
+      stages: [
+        {
+          order: 1,
+          stageTitle: 'Problems',
+          hoverText: 'I have 99 problems...',
+        },
+        {
+          order: 2,
+          stageTitle: 'Inputs-Activities',
+          hoverText: 'Insert coin here.'
+        },
+        {
+          order: 3,
+          stageTitle: 'Output',
+          hoverText: 'Point this way forward.'
+
+        },
+        {
+          order: 4,
+          stageTitle: 'Intermediate Outcome',
+          hoverText: 'Short-term gain'
+        },
+        { order: 5,
+          stageTitle: 'Final Outcome',
+          hoverText: 'Profit'
+        }
+      ]
+    }
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="stages"> 
+        {
+          stages.stages.map((stage) => {
+            return (
+              <div className="stage"> 
+                <h2 className="stage-title" title={stage.stageText}>{stage.stageTitle}</h2>
+              </div>
+            );
+          })
+        }
+        </div>
       </div>
     );
   }

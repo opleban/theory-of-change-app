0c1d7aec73b6db4597d088bc19259eaa93dbbfa9
Jonathan Pleban <opleban@gmail.com>
2019-01-26 21:18:30 -0500

    Updates

---
+++ precache-manifest.3d2e978029a652070c4b67986bfe1967.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "52dcd695f4ac81258ea0",
+    "url": "/theory-of-change-app/static/js/runtime~main.52dcd695.js"
+  },
+  {
+    "revision": "1a4457279121369e4c3f",
+    "url": "/theory-of-change-app/static/js/main.1a445727.chunk.js"
+  },
+  {
+    "revision": "c1febbd62e9fe2939684",
+    "url": "/theory-of-change-app/static/js/1.c1febbd6.chunk.js"
+  },
+  {
+    "revision": "1a4457279121369e4c3f",
+    "url": "/theory-of-change-app/static/css/main.acefdd01.chunk.css"
+  },
+  {
+    "revision": "c1febbd62e9fe2939684",
+    "url": "/theory-of-change-app/static/css/1.2ce19c6e.chunk.css"
+  },
+  {
+    "revision": "17ac922231db2a40af415288ccb577f2",
+    "url": "/theory-of-change-app/index.html"
+  }
+];

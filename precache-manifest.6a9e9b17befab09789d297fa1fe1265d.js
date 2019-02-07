1e4d1781284267346d0f356fc5b251064f4c56b6
Jonathan Pleban <opleban@gmail.com>
2019-02-06 22:48:41 -0500

    Updates

--- precache-manifest.6a9e9b17befab09789d297fa1fe1265d.js
+++ precache-manifest.6a9e9b17befab09789d297fa1fe1265d.js
@@ -1,15 +1,15 @@
 self.__precacheManifest = [
   {
-    "revision": "128310ec4fe7121145778d25ebc783fe",
+    "revision": "4a7458a6106791e81fd3af32c9788dfa",
     "url": "/theory-of-change-app/index.html"
   },
   {
-    "revision": "c1c6929669e7a3eae42a",
+    "revision": "b55f2f8338d778a0a7d0",
     "url": "/theory-of-change-app/static/css/main.6327f5e8.chunk.css"
   },
   {
-    "revision": "c1c6929669e7a3eae42a",
-    "url": "/theory-of-change-app/static/js/main.c1c69296.chunk.js"
+    "revision": "b55f2f8338d778a0a7d0",
+    "url": "/theory-of-change-app/static/js/main.b55f2f83.chunk.js"
   },
   {
     "revision": "e2302130c9f3ed875e79",
@@ -24,23 +24,23 @@ self.__precacheManifest = [
     "url": "/theory-of-change-app/static/css/1.220fb26f.chunk.css"
   },
   {
-    "revision": "128310ec4fe7121145778d25ebc783fe",
+    "revision": "4a7458a6106791e81fd3af32c9788dfa",
     "url": "/theory-of-change-app/san-fermin/index.html"
   },
   {
-    "revision": "128310ec4fe7121145778d25ebc783fe",
+    "revision": "4a7458a6106791e81fd3af32c9788dfa",
     "url": "/theory-of-change-app/participatory-budget/index.html"
   },
   {
-    "revision": "b495009b2613253832585c93a482eca0",
+    "revision": "9de0d92e4519efb1d814156efff4c4fd",
     "url": "/theory-of-change-app/es/san-fermin/index.html"
   },
   {
-    "revision": "b495009b2613253832585c93a482eca0",
+    "revision": "9de0d92e4519efb1d814156efff4c4fd",
     "url": "/theory-of-change-app/es/participatory-budget/index.html"
   },
   {
-    "revision": "b495009b2613253832585c93a482eca0",
+    "revision": "9de0d92e4519efb1d814156efff4c4fd",
     "url": "/theory-of-change-app/es.html"
   }
 ];
